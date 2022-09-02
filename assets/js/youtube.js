7263f93509345511555f355291af246a01d52db9
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-09-01 19:12:52 -0600

    new fixes

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,36 +1,3 @@
-var API_KEY = "AIzaSyBXT5ItW1SRT6wG4faRYr2wamNpvnyDy0w";
-var search = "";
-var maxResults=1;
-
-search = $(".search-input").val();
-
-$("#duration").change(function () {
-  duration = $(this).children("option:selected").val();
-});
-$("#order").change(function () {
-  order = $(this).children("option:selected").val();
-});
-
-$("#myForm").submit(function (event) {
-  e.preventDefault();
-
-  var url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
-      &part=snippet&q=${search}&maxResults=${maxResults}&type=video`;
-
-  $.ajax({
-    method: "GET",
-    url: url,
-    beforeSend: function () {
-      $("#btn").attr("disabled", true);
-      $("#results").empty();
-    },
-    success: function (data) {
-      console.log(data);
-      $("#btn").attr("disabled", false);
-      displayVideos(data);
-    },
-  });
-});
 
  var tag = document.createElement('script');
       
